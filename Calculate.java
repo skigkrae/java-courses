4ebe51c078e9e3c792cb96653b31c9b4959ccae0
skigkrae <igorkrajev@mail.ru>
2016-02-08 00:14:04 +0100

    Updated

--- Calculate.java
+++ Calculate.java
@@ -4,6 +4,18 @@ public class Calculate {
 		int first = Integer.valueOf(args[0]);
 		int second = Integer.valueOf(args[1]);
 		int sum = first + second;
-		System.out.println("Sum "  + sum);
+		System.out.println("Sum: "  + sum);
+		
+		int subtraction = first - second;
+		System.out.println("Substraction: " + subtraction);
+		
+		int multiplication = first * second;
+		System.out.println("Multiplication: " + multiplication);
+		
+		int division = first/second;
+		System.out.println("Division: " + division);
+		
+		int power = (int)Math.pow(first, second);
+		System.out.println(first + " to the power " + second + ": " + power);
 	}
 }	
